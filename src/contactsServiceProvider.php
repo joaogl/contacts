9d575f101773638bd572118a36eb224706c09a1b
Joaogl <joaoguerralourenco@hotmail.com>
2016-01-13 22:06:08 +0000

    Inicial commit

---
+++ src/contactsServiceProvider.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace jlourenco\contacts;
+
+use Illuminate\Support\ServiceProvider;
+
+class contactsServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // Publish our routes
+        // if (!$this->app->routesAreCached())
+		require __DIR__ . '/routes.php';
+
+        // Publish our views
+        $this->loadViewsFrom(base_path("resources/views"), 'contacts');
+        $this->publishes([
+            __DIR__ .  '/views' => base_path("resources/views")
+        ]);
+
+        // Publish our migrations
+        $this->publishes([
+            __DIR__ .  '/migrations' => base_path("database/migrations")
+        ], 'migrations');
+
+        // Publish a config file
+        $this->publishes([
+            __DIR__ . '/config' => base_path('/config')
+        ], 'config');
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // Bind the
+        $this->app->bind('contacts', function(){
+            return new contacts;
+        });
+    }
+}
