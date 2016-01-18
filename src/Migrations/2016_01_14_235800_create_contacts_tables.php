c56823e15900b73ef8a0c2365075e7f607551974
Joaogl <joaoguerralourenco@hotmail.com>
2016-01-18 12:46:53 +0000

    Updated migrations

--- src/Migrations/2016_01_14_235800_create_contacts_tables.php
+++ src/Migrations/2016_01_14_235800_create_contacts_tables.php
@@ -17,12 +17,12 @@ class CreateContactsTables extends Migration
         Schema::create('ContactType', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name', 100);
-            $table->string('icon', 50)->nullable();
-            $table->string('color_c', 6)->nullable();
-            $table->string('color_hc', 6)->nullable();
+            $table->text('html')->nullable();
             $table->string('description', 250);
+
             $table->timestamps();
             $table->softDeletes();
+            $table->creation();
         });
 
         Schema::create('Contact', function (Blueprint $table) {
@@ -30,12 +30,25 @@ class CreateContactsTables extends Migration
             $table->string('contact_reference', 50);
             $table->integer('contacttype')->unsigned();
             $table->string('contact', 250);
-            $table->tinyInteger('visibility');
+            $table->text('permissions');
+
             $table->timestamps();
             $table->softDeletes();
             $table->creation();
 
             $table->foreign('contacttype')->references('id')->on('ContactType');
+        });
+
+        Schema::create('Contact_Entity', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('contact')->unsigned();
+            $table->morphs("entity");
+
+            $table->timestamps();
+            $table->softDeletes();
+            $table->creation();
+
+            $table->foreign('contact')->references('id')->on('Contact');
         });
 
     }
